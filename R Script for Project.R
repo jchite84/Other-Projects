e2011133448228a6f13efe1ccddd8715c8a7338f
ross <ross>
2016-04-15 15:50:38 -0400

    Started For Loop for Subtracting the rolling average. Does not quite work. May need to calculate manually and double check results.

--- R Script for Project.R
+++ R Script for Project.R
@@ -1,6 +1,11 @@
- library(zoo)
- df <- read.csv(choose.files())
- 
- ShortRollAvg <- as.data.frame(rollmean(df[,3], 13))
- LongRollAvg <- as.data.frame(rollmean(df[,3], 62))
- 
+library(zoo)
+df <- read.csv(choose.files())
+
+#Works
+ShortRollAvg <- as.data.frame(rollmean(df[,3], 13))
+LongRollAvg <- as.data.frame(rollmean(df[,3], 62))
+
+#Does Not Quite Work - But is Close 
+for (i in 1:length(LongRollAvg[,1])){
+  rounded[i,]<- as.data.frame((LongRollAvg[i+25,]-LongRollAvg[(i-25),])*2)
+}
