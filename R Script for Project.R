0dc0e86f663cb67aab77d6b0fdc87408cb0aae91
ross <ross>
2016-04-18 10:42:35 -0400

    Got lagged subtraction function to work.

--- R Script for Project.R
+++ R Script for Project.R
@@ -4,8 +4,7 @@ df <- read.csv(choose.files())
 #Works
 ShortRollAvg <- as.data.frame(rollmean(df[,3], 13))
 LongRollAvg <- as.data.frame(rollmean(df[,3], 62))
+LongLCycle <- as.data.frame(2*diff(as.matrix(LongRollAvg), lag = 26))
 
-#Does Not Quite Work - But is Close 
-for (i in 1:length(LongRollAvg[,1])){
-  rounded[i,]<- as.data.frame((LongRollAvg[i+25,]-LongRollAvg[(i-25),])*2)
-}
+
+
